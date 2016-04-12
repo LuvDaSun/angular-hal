663fea7795e858a9f20388e69e55c4776ae21b62
Jonatan Männchen <jonatan.maennchen@ibrows.ch>
2016-04-12 10:43:22 +0000

    Cleanup Interceptor Config / Module

--- src/http-interception/http-interception.config.js
+++ src/http-interception/http-interception.config.js
@@ -1,10 +1,12 @@
 'use strict';
 
+import ResourceHttpInterceptorFactory from './resource-http-interceptor.factory';
+
 /**
  * @param {HttpProvider} $httpProvider
  */
 export default function HttpInterceptorConfiguration($httpProvider) {
-  $httpProvider.interceptors.push('ResourceHttpInterceptor');
+  $httpProvider.interceptors.push(ResourceHttpInterceptorFactory);
 }
 
 HttpInterceptorConfiguration.$inject = [
--- src/http-interception/index.js
+++ src/http-interception/index.js
@@ -2,11 +2,9 @@
 
 const MODULE_NAME = 'angular-hal.http-interception';
 
-
 import resource from '../resource';
 import configuration from '../configuration';
 
-import ResourceHttpInterceptor from './resource-http-interceptor.factory';
 import HttpInterceptorConfiguration from './http-interception.config';
 
 // Add module for http interception
@@ -17,8 +15,6 @@ angular
   ])
 
   .config(HttpInterceptorConfiguration)
-
-  .factory('ResourceHttpInterceptor', ResourceHttpInterceptor)
 ;
 
 export default MODULE_NAME;
