6b6848e5aa2d7a2ea4cc177220e7861c8fce853c
Jonatan Männchen <jonatan.maennchen@ibrows.ch>
2016-04-27 12:50:03 +0000

    Test Jspm, Browserify & Webpack Seperate

---
+++ config.js
@@ -0,0 +1,63 @@
+/* globals System: false */
+'use strict';
+
+System.config({
+  baseURL: "/base",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "optional": [
+      "runtime",
+      "optimisation.modules.system"
+    ]
+  },
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "babel": "npm:babel-core@5.8.38",
+    "babel-runtime": "npm:babel-runtime@5.8.38",
+    "content-type": "npm:content-type@1.0.1",
+    "core-js": "npm:core-js@1.2.6",
+    "rfc6570": "npm:rfc6570@0.1.8",
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:babel-runtime@5.8.38": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:core-js@1.2.6": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    }
+  }
+});
---
+++ karma.jspm.conf.js
@@ -0,0 +1,38 @@
+/* jshint node:true */
+/* eslint-env node */
+'use strict';
+
+module.exports = function(config) {
+  config.set({
+    basePath: '.',
+    frameworks: ['jspm', 'jasmine'],
+    files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+//      'test/**/*.spec.js',
+    ],
+
+
+    reporters: [
+      'dots',
+    ],
+
+    jspm: {
+      loadFiles: [
+        'test/**/*.spec.js',
+      ],
+      serveFiles: [
+        'test/**/!(*.spec).js',
+        'src/**/*.js',
+      ],
+    },
+
+    proxies: {
+      '/local/': 'http://localhost:8080/',
+    },
+
+    autoWatch: true,
+    browsers: ['PhantomJS']
+  });
+
+};
